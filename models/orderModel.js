2741e75e07b0f39d9cf1261622d3b4b22f8e8fd4
rashedabir <rashed15-2155@diu.edu.bd>
2021-08-15 15:36:04 +0600

    order database setup

---
+++ models/orderModel.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      require: true,
+      trim: true,
+    },
+    phone: {
+      type: String,
+      require: true,
+      trim: true,
+    },
+    address: {
+      type: String,
+      require: true,
+      trim: true,
+    },
+    quantity: {
+      type: String,
+      require: true,
+      trim: true,
+    },
+    status: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Order", orderSchema);
